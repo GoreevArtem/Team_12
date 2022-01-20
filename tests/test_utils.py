e1411ababf7dc24e63ca2d303d8d8eee0a736c62
Lirak150 <Lirak150@yandex.ru>
2022-01-20 20:47:00 +0300

    Added test for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+from utils import Utils
+
+
+class TestUtils(unittest.TestCase):
+    """Test for class Utils"""
+
+    def test_clear_dir(self):
+        """Test for clean_dir in Utils"""
+        num_files = 10
+        file_dir = "test_directory"
+        for i in range(num_files):
+            with open(f"{file_dir}/{i}.bin", "wb") as file:
+                file.write(i.to_bytes(byteorder="little", length=8))
+        self.assertEqual(len(os.listdir(file_dir)), num_files)
+        Utils.clear_dir(file_dir)
+        self.assertEqual(len(os.listdir(file_dir)), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
